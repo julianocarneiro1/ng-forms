3bf31a5243e766f752192fb0ba5bc37ec84f1681
Juliano <julianoomc@gmail.com>
2021-09-28 13:40:59 -0300

    Form posting using HttpClient

---
+++ src/app/data/data.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { UserSettings } from './user-settings';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient) { }
+
+  postUserSettingsForm(userSettings: UserSettings): Observable<any> {
+    
+    return this.http.post('https://putsreq.com/dgGBr7u2aOpThg2WksLH', userSettings)
+    
+    //return of(userSettings)
+  }
+}
