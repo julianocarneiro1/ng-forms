06a71560cf2d6c49f40a4713932ceb7c19e1d655
Juliano <julianoomc@gmail.com>
2021-09-28 11:51:19 -0300

    Handling form control events

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { NgForm, NgModel } from '@angular/forms';
 import { UserSettings } from '../data/user-settings';
 
 @Component({
@@ -24,8 +24,11 @@ export class UserSettingsFormComponent implements OnInit {
   ngOnInit() {
   }
 
+  onBlur(field: NgModel) {
+    console.log('in onBlur: ', field.valid)
+  }
+
   onSubmit(form: NgForm) {
     console.log('in onSubmit: ', form.valid)
   }
-
 }
