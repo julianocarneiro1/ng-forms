79b3799756de9cc32d59407649957f515c1476ec
Juliano <julianoomc@gmail.com>
2021-09-28 11:14:06 -0300

    Submitting form

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { UserSettings } from '../data/user-settings';
 
 @Component({
@@ -21,6 +22,10 @@ export class UserSettingsFormComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    console.log('in onSubmit: ', form.valid)
   }
 
 }
